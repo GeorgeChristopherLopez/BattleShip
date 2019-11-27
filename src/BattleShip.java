95a0a40171f4d9c9f0522cc28adb8d8eb33d93bf
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2019-11-27 12:29:55 -0500

    fixed repeat firing bug
    can no long select 1,1,1 and get hit, hit, hit.
    ship locations are elements of ArrayList and are removed upon "hit".

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -1,5 +1,6 @@
-
+    import java.lang.reflect.Array;
     import java.util.Scanner;
+    import  java.util.ArrayList;
 
     public class BattleShip {
 
@@ -11,9 +12,17 @@
             //COMPUTE a random number between 0 and 4 that will be the starting location
             // cell position
             int start =(int) Math.floor(Math.random()*(5));
+
             Ship ship1 = new Ship();
-            int[] locations = {start,start+1,start+2};
+            ArrayList<String> locations = new ArrayList<String>();
+            // add start position to location arrayList
+            for (int x =0; x < 3; x++){
+
+                String loc = Integer.toString(start + x);
+                locations.add(loc);
+            }
             ship1.setLocationCells(locations);
+            System.out.println(locations);
             boolean isAlive = true;
 
 
@@ -31,58 +40,29 @@
         }
 
         static class Ship {
-            //DECLARE an int array to hold the location cells. Call it locationCells.
-            int[] locationCells;
+            private ArrayList<String> locationCells;
 
-            //  DECLARE an int to hold the number of hits. Call it numOfHits and SET it to 0.
-            int numOfHits;
+            public void setLocationCells(ArrayList<String> loc){
+                locationCells = loc;
+            }
 
-            // DECLARE a checkYourself() method that takes a String for the user’s guess (“1”, “3”, etc.),
-            //checks it, and returns a result representing a “hit”, “miss”, or “kill”.
+
             String checkYourself(String stringGuess){
-                //GET the user guess as a String parameter
-                //CONVERT the user guess to an int
-                int intGuess = Integer.parseInt(stringGuess);
                 String result = "miss";
-
-                //REPEAT with each of the location cells in the int array
-                //// COMPARE the user guess to the location cell
-                //IF the user guess matches
-                //INCREMENT the number of hits
-                for(int cell: locationCells){
-                    if(intGuess == cell){
+                int index =locationCells.indexOf(stringGuess);
+                if(index >=0) {
+                    locationCells.remove(index);
+                    if(locationCells.isEmpty()) {
+                        result = "kill";
+                    } else {
                         result = "hit";
-                        numOfHits++;
-                        break;
                     }
-
                 }
-
-
-                //// FIND OUT if it was the last location cell:
-                //IF number of hits is 3, RETURN “kill” as the result
-                //ELSE it was not a kill, so RETURN“hit”
-                //END IF
-                //ELSE the user guess did not match, so RETURN “miss”
-                if(numOfHits == locationCells.length){
-                    result = "kill";
-                }
-                //END IF
-                //END REPEAT
-                //END METHOD
                 System.out.println(result);
 
                 return result;
             }
 
-            //DECLARE a setLocationCells() setter method that takes an int array (which has the three cell
-            //locations as ints (2,3,4, etc.).
-            public void setLocationCells(int[] loc){
-                //GET the cell locations as an int array parameter
-                //ASSIGN the cell locations parameter to the cell locations instance variable
-                //END METHOD
-                locationCells = loc;
-            }
 
 
 
